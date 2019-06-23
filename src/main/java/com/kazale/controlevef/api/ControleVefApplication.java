80548510258a869d579a82313adc0620a2de8db6
John Pereira <johnev90@gmail.com>
2019-06-23 20:36:02 -0300

    Commit inicial do projeto.

---
+++ src/main/java/com/kazale/controlevef/api/ControleVefApplication.java
@@ -0,0 +1,13 @@
+package com.kazale.controlevef.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleVefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleVefApplication.class, args);
+	}
+
+}
